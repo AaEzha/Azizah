1a81710f4633f5085edaa23c676b6bf283df0c41
Aa Ezha <aaezha@gmail.com>
2015-11-17 18:00:51 +0700

    update iseng

--- letter.php
+++ letter.php
@@ -1,25 +1,42 @@
-<h1>Letter <small>Configuration</small></h1>
 <?php
 if(isset($_GET['type'])){
 	$type = $_GET['type'];
 	switch ($type) {
-		case 'value':
-			# code...
+		case 'confirm':
+			$title = "Confirm Clearance Letter";
+			break;
+
+		case 'reject':
+			$title = "Reject Clearance Letter";
+			break;
+
+		case 'assessment':
+			$title = "Assessment Letter";
+			break;
+
+		case 'finish':
+			$title = "End of Internship Letter";
 			break;
 		
 		default:
 			# code...
 			break;
 	}
+
+	echo "<h1>$title <small>Configuration</small></h1>";
 }else{
 ?>
+<h1>Letter <small>Configuration</small></h1>
 <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
 	<div class="panel panel-primary">
 		  <div class="panel-heading">
 				<h3 class="panel-title">Confirm Clearance Letter</h3>
 		  </div>
 		  <div class="panel-body">
-				<a href="?p=letter&type=accept" class="btn btn-primary btn-block">Edit</a>
+				<a href="?p=letter&type=confirm" class="btn btn-primary btn-block">Edit</a>
+		  </div>
+		  <div class="panel-footer">
+		  		<a href="#" class="btn btn-info btn-block">Preview</a>
 		  </div>
 	</div>
 </div>	
@@ -30,7 +47,10 @@ if(isset($_GET['type'])){
 				<h3 class="panel-title">Reject Clearance Letter</h3>
 		  </div>
 		  <div class="panel-body">
-				<a href="?p=letter&type=accept" class="btn btn-primary btn-block">Edit</a>
+				<a href="?p=letter&type=reject" class="btn btn-primary btn-block">Edit</a>
+		  </div>
+		  <div class="panel-footer">
+		  		<a href="#" class="btn btn-info btn-block">Preview</a>
 		  </div>
 	</div>
 </div>	
@@ -41,7 +61,10 @@ if(isset($_GET['type'])){
 				<h3 class="panel-title">Assessment Letter</h3>
 		  </div>
 		  <div class="panel-body">
-				<a href="?p=letter&type=accept" class="btn btn-primary btn-block">Edit</a>
+				<a href="?p=letter&type=assessment" class="btn btn-primary btn-block">Edit</a>
+		  </div>
+		  <div class="panel-footer">
+		  		<a href="#" class="btn btn-info btn-block">Preview</a>
 		  </div>
 	</div>
 </div>	
@@ -52,7 +75,10 @@ if(isset($_GET['type'])){
 				<h3 class="panel-title">End of Internship Letter</h3>
 		  </div>
 		  <div class="panel-body">
-				<a href="?p=letter&type=accept" class="btn btn-primary btn-block">Edit</a>
+				<a href="?p=letter&type=finish" class="btn btn-primary btn-block">Edit</a>
+		  </div>
+		  <div class="panel-footer">
+		  		<a href="#" class="btn btn-info btn-block">Preview</a>
 		  </div>
 	</div>
 </div>	
