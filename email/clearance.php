791b422e453d9e5ea7e05f5798219e313a863af2
Aa Ezha <aaezha@gmail.com>
2015-11-22 12:20:48 +0700

    email clearance&rejection

---
+++ email/clearance.php
@@ -0,0 +1,38 @@
+<?php
+require 'bin/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'ssl://smtp.googlemail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'donotreply.gmf.aeroasia@gmail.com';                 // SMTP username
+$mail->Password = 'tidakbolehkosong';                           // SMTP password
+$mail->Port = 465;                                    // TCP port to connect to
+
+$mail->setFrom('donotreply.gmf.aeroasia@gmail.com', 'GMF AeroAsia');
+$mail->addAddress($_SESSION['emailnya'], $_SESSION['namanya']);     // Add a recipient
+/*$mail->addReplyTo('info@example.com', 'Information');
+$mail->addCC('cc@example.com');
+$mail->addBCC('bcc@example.com');*/
+
+/*$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name*/
+
+$mail->isHTML(true);                                  // Set email format to HTML
+$mail->Subject = 'Welcome to GMF AeroAsia Internship Program';
+$mail->Body    = '<p>Hai '.$_SESSION['namanya'].', your internship program request has been granted. Congratulation!<p>
+                    <p>For further information and Clearance Letter, you should log in to GMF AeroAsia\'s Web with your own account.</p>
+					<p>Enjoy your account.</p>';
+$mail->AltBody = 'Hai '.$_SESSION['namanya'].', your internship program request has been granted. Congratulation! For further information and Clearance Letter, you should log in to GMF AeroAsia\'s Web with your own account. Enjoy your account.';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+    unset($_SESSION['namanya']);
+    unset($_SESSION['emailnya']);
+}
